5a04911f99dde1348d806261ad8f020313aea560
Adrizen89 <adrienberard@hotmail.fr>
2024-10-12 16:19:57 +0200

    Add api

--- js/api.js
+++ js/api.js
@@ -1,15 +1,39 @@
 
+// Utiliser la variable API_KEY
+const apiKey = "goldapi-60fhsm2646jeg-io";
 
-const updatePrices = () => {
+console.log(apiKey);
+var myHeaders = new Headers();
+myHeaders.append("x-access-token", apiKey);
+myHeaders.append("Content-Type", "application/json");
 
-    // A l'attendant l'api réelle 
-    const goldPrice = Math.random() * (60 - 50) + 50;
-    const silverPrice = Math.random() * (1.2 - 0.8) + 0.8;
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
 
-    document.getElementById('goldPrice').textContent = goldPrice.toFixed(2);
-    document.getElementById('silverPrice').textContent = silverPrice.toFixed(2);
+
+async function fetchAndDisplayChart() {
+    try {
+
+        const responseGold = await fetch("https://www.goldapi.io/api/XAU/USD", requestOptions);
+        const dataGold = await responseGold.json();
+        
+
+        const responseSilver = await fetch("https://www.goldapi.io/api/XAG/USD", requestOptions);
+        const dataSilver = await responseSilver.json();
+
+        // Afficher les prix dans les éléments HTML
+        document.getElementById('goldPrice').textContent = dataGold.price.toFixed(2);
+        document.getElementById('silverPrice').textContent = dataSilver.price.toFixed(2);
+    } catch (error) {
+        console.error('Erreur lors de la récupération des données:', error);
+        alert('Impossible de récupérer les données des cours. Veuillez réessayer plus tard.');
+    }
 }
 
-// Init et maj des valeurs toutes les minutes 
-updatePrices();
-setInterval(updatePrices, 60000);
+
+fetchAndDisplayChart();
+setInterval(fetchAndDisplayChart, 60000);
+
